0d31c384208402d630995ecfd0eb7ed85081f4ad
Tom Martin <>
2019-02-26 18:08:55 -0700

    trying to fix cors

--- server.js
+++ server.js
@@ -6,7 +6,21 @@ const port = process.env.PORT || 8000;
 const cors = require("cors");
 const knex = require('./db/knex.js');
 require('dotenv').config();
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
